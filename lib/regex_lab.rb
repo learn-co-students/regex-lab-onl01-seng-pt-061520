cd8a3217ff33ae6f66263c318013d3073ab4a626
IgoVeyner <igorveyner95@gmail.com>
2020-06-29 12:04:54 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,36 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+    if word.match(/^[aeiouAEIOU]/) != nil
+        return true
+    else
+        return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.match(/^[A-Z].*\W$/) != nil
+        return true
+    else 
+        return false
+    end
 end
 
 def valid_phone_number?(phone)
-
+    if phone.match(/[\d]{10}|\(\d{3}\)\d{3}\-\d{4}\b|\d{3}\s\d{3}\s\d{4}|\(\d{3}\)\d{7}/) != nil
+        return true
+    else 
+        return false
+    end
 end
+
+# binding.pry
+0
